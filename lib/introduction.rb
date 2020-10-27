5ddb29598e1c84a946279d79e4eb5f2008dc66c7
Veresia Hiralall <hiralallveresia@yahoo.com>
2020-10-27 19:01:07 -0400

    commit tree 009df60d3b0457abd3f68d2a4f5d317cce08eeb6

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction(name)
+    puts "Hi, my name is #{name}."
+    end 
+introduction("Josh")
+
+def introduction_with_language(name, program)
+    puts "Hi, my name is #{name} and I am learning to program in #{program}."
+    end 
+
+introduction_with_language("Dan", "Ember")
+
